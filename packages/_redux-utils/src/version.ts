972e21ab5583400bc979a46aad684e6f2b3d54fd
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-01-08 14:31:57 +0100

    fix dependencies of schematics

--- packages/_redux-utils/src/version.ts
+++ packages/_redux-utils/src/version.ts
@@ -1,4 +1,4 @@
 /** Version and build number of the package */
-export const VERSION = {version: {major: '9', minor: '0', patch: '0'}, build: new Date(1578489050785)};
+export const VERSION = {version: {major: '9', minor: '0', patch: '0'}, build: new Date(1578490257973)};
 /** Module name */
 export const MODULE = '@ibm-wch-sdk/redux-utils';
