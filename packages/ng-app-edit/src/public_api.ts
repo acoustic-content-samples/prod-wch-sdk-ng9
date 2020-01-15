3ba3051705821dafa2472810ff13bfd3cf26a1a3
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-01-15 13:44:50 +0100

    add doc for redux-feature-auth-content

--- packages/ng-app-edit/src/public_api.ts
+++ packages/ng-app-edit/src/public_api.ts
@@ -1,5 +1,5 @@
 /**
- * Module that contains a default configuration to run an [Angular](https://angular.io/) application against [Acoustic Content](https://www.ibm.com/products/watson-content-hub)  in edit mode.
+ * Module that contains a default configuration to run an {@link https://angular.io/|Angular} application against {@link https://www.ibm.com/products/watson-content-hub|Acoustic Content}  in edit mode.
  *
  * @packageDocumentation
  */
--- packages/redux-feature-auth-content/src/public_api.ts
+++ packages/redux-feature-auth-content/src/public_api.ts
@@ -1,3 +1,9 @@
-export * from "./state/auth-content/index";
-export { migrateContentItems } from "./utils/auth.content.migrate";
-export * from "./version";
+/**
+ * Implementation of a redux feature to manage authoring content.
+ *
+ * @packageDocumentation
+ */
+
+export * from './state/auth-content/index';
+export { migrateContentItems } from './utils/auth.content.migrate';
+export * from './version';
