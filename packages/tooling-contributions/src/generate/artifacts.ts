5984427bfa82cf9c8c0b7c51053e414ee0082f70
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-01-30 12:50:17 +0100

    driver split

---
+++ packages/tooling-contributions/src/generate/artifacts.ts
@@ -0,0 +1,490 @@
+import {
+  AuthoringContentItem,
+  CLASSIFICATION_CONTENT,
+  ELEMENT_TYPE_GROUP,
+  ELEMENT_TYPE_OPTION_SELECTION,
+  ELEMENT_TYPE_REFERENCE,
+  ELEMENT_TYPE_TEXT,
+  Status
+} from '@acoustic-content-sdk/api';
+import {
+  createFileDescriptor,
+  createGuid,
+  createRevision,
+  FileDescriptor,
+  ReadDirectory,
+  ReadTextFile
+} from '@acoustic-content-sdk/tooling';
+import {
+  arrayPush,
+  assertArray,
+  filterArray,
+  isNotEmpty,
+  mapArray,
+  opShareLast,
+  reduceArray,
+  reduceForIn,
+  rxPipe,
+  toArray
+} from '@acoustic-content-sdk/utils';
+import { JSDOM } from 'jsdom';
+import { from, merge, Observable, OperatorFunction } from 'rxjs';
+import {
+  map,
+  mergeMap,
+  pluck,
+  shareReplay,
+  toArray as rxToArray
+} from 'rxjs/operators';
+import { ArtifactMode } from './schema';
+
+export type Artifact = AuthoringContentItem;
+
+/**
+ * Extracts the style elements from an element
+ *
+ * @param aElement - the element
+ * @returns the style links
+ */
+function selectStyleLinks(aElement: HTMLElement): Observable<HTMLLinkElement> {
+  return from(
+    toArray(
+      aElement.querySelectorAll<HTMLLinkElement>('link[rel="stylesheet"]')
+    )
+  );
+}
+
+/**
+ * Extracts the script elements from an element
+ *
+ * @param aElement - the element
+ * @returns the script elements
+ */
+function selectScripts(aElement: HTMLElement): Observable<HTMLScriptElement> {
+  return from(
+    toArray(aElement.querySelectorAll<HTMLScriptElement>('script[src]'))
+  );
+}
+
+const NAME_MATCH = /^([\w-_]+)(\.\w+)?(\.\w+)$/;
+
+function createBaseName(aName: string): string {
+  return aName.replace(NAME_MATCH, '$1$3');
+}
+
+const PAGE_CONTRIBUTIONS_ID = 'aab86460-0018-44c2-9f52-3a326e58f7f7';
+const PAGE_CONTRIBUTION_ID = '354743b2-f89a-482b-b447-2b5a2367c8bd';
+const STYLE_CONTRIBUTION_ID = '3a3726b8-e0b2-4f10-98ed-b1c82e851b99';
+const SCRIPT_CONTRIBUTION_ID = '5c473463-db07-4132-b8a1-bd40cbdf4b18';
+
+function createName(aLink: string, aBaseName: string): string {
+  const baseName = createBaseName(aLink);
+  return `${aBaseName} - ${baseName}`;
+}
+
+/**
+ * Construct a new content item based on the style link
+ *
+ */
+function createStyleItem(
+  aStyle: HTMLLinkElement,
+  aBaseName: string,
+  aBasePath: string,
+  tags: string[]
+): AuthoringContentItem {
+  // url
+  const href = aStyle.href;
+  const name = createName(href, aBaseName);
+  const id = createGuid(name);
+  const path = `${aBasePath}/${href}`;
+  const key = createGuid(path);
+  // construct the item
+  const item = {
+    id,
+    name,
+    tags,
+    classification: CLASSIFICATION_CONTENT,
+    typeId: STYLE_CONTRIBUTION_ID,
+    elements: {
+      href: {
+        elementType: ELEMENT_TYPE_TEXT,
+        value: path
+      },
+      key: {
+        elementType: ELEMENT_TYPE_TEXT,
+        value: key
+      }
+    },
+    status: Status.READY
+  };
+  // ok
+  return item as any;
+}
+
+/**
+ * Construct a new content item based on the style link
+ *
+ */
+function createScriptItem(
+  aScript: HTMLScriptElement,
+  aBaseName: string,
+  aBasePath: string,
+  tags: string[]
+): AuthoringContentItem {
+  // url
+  const src = aScript.src;
+  const name = createName(src, aBaseName);
+  const id = createGuid(name);
+  const path = `${aBasePath}/${src}`;
+  const key = createGuid(path);
+  // check for the type
+  const selection = aScript.hasAttribute('nomodule')
+    ? 'nomodule'
+    : 'application/javascript';
+  // construct the item
+  const item = {
+    id,
+    name,
+    tags,
+    classification: CLASSIFICATION_CONTENT,
+    typeId: SCRIPT_CONTRIBUTION_ID,
+    elements: {
+      src: {
+        elementType: ELEMENT_TYPE_TEXT,
+        value: path
+      },
+      type: {
+        elementType: ELEMENT_TYPE_OPTION_SELECTION,
+        value: {
+          selection
+        }
+      },
+      key: {
+        elementType: ELEMENT_TYPE_TEXT,
+        value: key
+      }
+    },
+    status: Status.READY
+  };
+  // ok
+  return item as any;
+}
+
+function createHeadArtifacts(
+  aHead: HTMLElement,
+  aBaseName: string,
+  aBasePath: string,
+  tags: string[]
+): Observable<AuthoringContentItem> {
+  // head links
+  const style$ = rxPipe(
+    selectStyleLinks(aHead),
+    map((link) => createStyleItem(link, aBaseName, aBasePath, tags))
+  );
+  // head scripts
+  const script$ = rxPipe(
+    selectScripts(aHead),
+    map((script) => createScriptItem(script, aBaseName, aBasePath, tags))
+  );
+  // merge
+  return merge(style$, script$);
+}
+
+function createBodyArtifacts(
+  aBody: HTMLElement,
+  aBaseName: string,
+  aBasePath: string,
+  tags: string[]
+): Observable<AuthoringContentItem> {
+  // body scripts
+  return rxPipe(
+    selectScripts(aBody),
+    map((script) => createScriptItem(script, aBaseName, aBasePath, tags))
+  );
+}
+
+/**
+ * Adds the contribution for the app root element
+ *
+ * @param selection - the selection mode
+ * @returns the element
+ */
+function appRootContribution(selection: string) {
+  return {
+    embed: {
+      elementType: ELEMENT_TYPE_TEXT,
+      values: ['<app-root></app-root>']
+    },
+    mode: {
+      elementType: ELEMENT_TYPE_OPTION_SELECTION,
+      value: {
+        selection
+      }
+    }
+  };
+}
+
+interface AuthoringIdRef {
+  id: string;
+  mode: ArtifactMode;
+}
+
+function reference(aItem: ItemWithMode): AuthoringIdRef {
+  return {
+    id: aItem.item.id,
+    mode: aItem.mode
+  };
+}
+
+/**
+ * Organize the IDs by mode
+ *
+ * @param aValues - the authoring IDs
+ * @returns the values organized by mode
+ */
+function byMode(aValues: AuthoringIdRef[]): Record<string, string[]> {
+  // reduce
+  return reduceArray(
+    aValues,
+    (aDst: Record<string, string[]>, aRef: AuthoringIdRef) => {
+      // insert
+      arrayPush(aRef.id, assertArray(aRef.mode, aDst));
+      return aDst;
+    },
+    {}
+  );
+}
+
+/**
+ * Generates a reproducible guid for the configuration
+ *
+ * @param selection  - selection
+ * @param values - reference values
+ *
+ * @returns the guid
+ */
+const createReferenceContributionsGuid = (
+  aSelection: string,
+  aValues: string[]
+) => createGuid([aSelection, ...aValues].join('/'));
+
+function referenceContributions(selection: string, values: string[]) {
+  // generate a key
+  const key = createReferenceContributionsGuid(selection, values);
+  // the reference
+  return {
+    reference: {
+      values: mapArray(values, (id) => ({ id })),
+      elementType: ELEMENT_TYPE_REFERENCE
+    },
+    mode: {
+      elementType: ELEMENT_TYPE_OPTION_SELECTION,
+      value: {
+        selection
+      }
+    },
+    key: {
+      elementType: ELEMENT_TYPE_TEXT,
+      value: key
+    }
+  };
+}
+
+/**
+ * Generates a reference contribution from all items on the stream
+ *
+ * @param aMode - the mode
+ * @returns the operation that creates the contribution
+ */
+function referenceContribution(): OperatorFunction<ItemWithMode, any> {
+  return (item$) =>
+    rxPipe(
+      item$,
+      map(reference),
+      rxToArray(),
+      map(byMode),
+      map((values) =>
+        reduceForIn(
+          values,
+          (dst: any[], val: string[], mode: string) =>
+            arrayPush(referenceContributions(mode, val), dst),
+          []
+        )
+      ),
+      mergeMap((all) => from(all))
+    );
+}
+
+function createPageContribution(values: any[]) {
+  return {
+    values,
+    elementType: ELEMENT_TYPE_GROUP,
+    typeRef: {
+      id: PAGE_CONTRIBUTION_ID
+    }
+  };
+}
+
+export function headContribution(): OperatorFunction<ItemWithMode, any> {
+  return (item$) =>
+    rxPipe(
+      item$,
+      referenceContribution(),
+      rxToArray(),
+      map(createPageContribution)
+    );
+}
+
+export function bodyContribution(): OperatorFunction<ItemWithMode, any> {
+  return (item$) =>
+    rxPipe(
+      item$,
+      referenceContribution(),
+      //      startWith(appRootContribution(aMode)),
+      rxToArray(),
+      map(createPageContribution)
+    );
+}
+
+export function createPageContributions(
+  aProjectName: string,
+  head: any,
+  body: any,
+  tags: string[]
+): AuthoringContentItem {
+  // generate some ids
+  const name = `${aProjectName} - Profile`;
+  const id = createGuid(name);
+  const key = id;
+
+  const item = {
+    tags,
+    classification: CLASSIFICATION_CONTENT,
+    elements: {
+      head,
+      body,
+      key: {
+        elementType: ELEMENT_TYPE_TEXT,
+        value: key
+      }
+    },
+    name,
+    typeId: PAGE_CONTRIBUTIONS_ID,
+    id,
+    status: Status.READY
+  };
+  // some magic casting
+  return item as any;
+}
+
+export function addRevision<T>(aValue: T): T {
+  // patch
+  const value = aValue as any;
+  value.rev = createRevision(aValue);
+  // ok
+  return value;
+}
+
+export const DEFAULT_MODE = `${ArtifactMode.LIVE},${ArtifactMode.PREVIEW}`;
+
+export interface ModeConfig {
+  /**
+   * The mode
+   */
+  mode: ArtifactMode;
+  /**
+   * The matching output path
+   */
+  outputPath: string;
+}
+
+interface ItemWithMode {
+  mode: ArtifactMode;
+  item: AuthoringContentItem;
+}
+
+const createItemWithMode = (
+  mode: ArtifactMode,
+  item: AuthoringContentItem
+): ItemWithMode => ({ mode, item });
+
+interface Artifacts {
+  head$: Observable<ItemWithMode>;
+  body$: Observable<ItemWithMode>;
+}
+
+/**
+ * Constructs the artifacts for a particular root
+ *
+ * @param aConfig - the config
+ * @param aRootDir  - root directory
+ * @param aProjectName - name of the project
+ * @param aTags - the tags for the artifacts
+ * @param aReadTextFile - read callback
+ *
+ * @returns the contributions
+ */
+export function createArtifacts(
+  aConfig: ModeConfig,
+  aRootDir: string,
+  aProjectName: string,
+  aTags: string[],
+  aReadTextFile: ReadTextFile
+): Artifacts {
+  // decode the config
+  const { mode, outputPath } = aConfig;
+  // the base name
+  const baseName = `${aProjectName} - ${mode}`;
+  // index
+  const indexPath = `${aRootDir}${outputPath}/index.html`;
+  // parse the index
+  const dom$ = rxPipe(
+    aReadTextFile(indexPath),
+    map((data) => new JSDOM(data)),
+    map((jsdom) => jsdom.window.document),
+    opShareLast
+  );
+  // head and body contributions
+  const head$ = rxPipe(
+    dom$,
+    pluck('head'),
+    mergeMap((el) => createHeadArtifacts(el, baseName, outputPath, aTags)),
+    map((item) => createItemWithMode(mode, item)),
+    shareReplay()
+  );
+  const body$ = rxPipe(
+    dom$,
+    pluck('body'),
+    mergeMap((el) => createBodyArtifacts(el, baseName, outputPath, aTags)),
+    map((item) => createItemWithMode(mode, item)),
+    shareReplay()
+  );
+  // returns the artifacts
+  return { head$, body$ };
+}
+
+/**
+ * Splits the array into individual values
+ *
+ * @param aValue - the value, separated by ','
+ * @returns the split array
+ */
+export const splitArray = (aValue: string): string[] =>
+  filterArray(
+    mapArray(aValue.split(','), (m) => m.trim()),
+    isNotEmpty
+  );
+
+export function readFilesForConfig(
+  aRoot: string,
+  aConfig: ModeConfig,
+  aReadDir: ReadDirectory
+): Observable<FileDescriptor<Buffer>> {
+  // target directory
+  const outDir = `${aRoot}${aConfig.outputPath}`;
+  // scan the files
+  return rxPipe(
+    aReadDir(outDir),
+    map(([name, buffer]) => createFileDescriptor(`${outDir}${name}`, buffer))
+  );
+}
--- packages/tooling-contributions/src/generate/ng-driver/driver.ts
+++ packages/tooling-contributions/src/generate/ng-driver/driver.ts
@@ -1,18 +1,6 @@
-import {
-  AuthoringContentItem,
-  CLASSIFICATION_CONTENT,
-  ELEMENT_TYPE_GROUP,
-  ELEMENT_TYPE_OPTION_SELECTION,
-  ELEMENT_TYPE_REFERENCE,
-  ELEMENT_TYPE_TEXT,
-  Status
-} from '@acoustic-content-sdk/api';
 import { rxCacheSingle } from '@acoustic-content-sdk/rx-utils';
 import {
   canonicalizeJson,
-  createFileDescriptor,
-  createGuid,
-  createRevision,
   ensureDirPath,
   FileDescriptor,
   ProjectType,
@@ -26,404 +14,42 @@ import {
 } from '@acoustic-content-sdk/tooling';
 import {
   anyToString,
-  arrayPush,
-  assertArray,
-  filterArray,
   getPath,
   isNotEmpty,
   JSONObject,
   mapArray,
   opShareLast,
   pluckProperty,
-  reduceArray,
-  reduceForIn,
-  rxPipe,
-  toArray
+  rxPipe
 } from '@acoustic-content-sdk/utils';
-import { JSDOM } from 'jsdom';
-import { combineLatest, from, merge, Observable, OperatorFunction } from 'rxjs';
-import {
-  first,
-  map,
-  mergeMap,
-  pluck,
-  shareReplay,
-  toArray as rxToArray
-} from 'rxjs/operators';
+import { combineLatest, from, merge, Observable } from 'rxjs';
+import { first, map, mergeMap } from 'rxjs/operators';
 
+import {
+  addRevision,
+  Artifact,
+  bodyContribution,
+  createArtifacts,
+  createPageContributions,
+  DEFAULT_MODE,
+  headContribution,
+  ModeConfig,
+  readFilesForConfig,
+  splitArray
+} from '../artifacts';
 import { ArtifactMode } from './../schema';
 import { CreateNgDriverArtifactsSchema } from './schema';
 
-export type Artifact = AuthoringContentItem;
+const DEFAULT_CONFIGURATION = 'production';
 
-const selectOutputPath = pluckProperty<JSONObject, 'outputPath'>('outputPath');
+const DEFAULT_TARGET = 'build';
 
 const selectRootPath = pluckProperty<
   WorkspaceProject<ProjectType.Application>,
   'root'
 >('root', '');
 
-/**
- * Extracts the style elements from an element
- *
- * @param aElement - the element
- * @returns the style links
- */
-function selectStyleLinks(aElement: HTMLElement): Observable<HTMLLinkElement> {
-  return from(
-    toArray(
-      aElement.querySelectorAll<HTMLLinkElement>('link[rel="stylesheet"]')
-    )
-  );
-}
-
-/**
- * Extracts the script elements from an element
- *
- * @param aElement - the element
- * @returns the script elements
- */
-function selectScripts(aElement: HTMLElement): Observable<HTMLScriptElement> {
-  return from(
-    toArray(aElement.querySelectorAll<HTMLScriptElement>('script[src]'))
-  );
-}
-
-const NAME_MATCH = /^([\w-_]+)(\.\w+)?(\.\w+)$/;
-
-function createBaseName(aName: string): string {
-  return aName.replace(NAME_MATCH, '$1$3');
-}
-
-const PAGE_CONTRIBUTIONS_ID = 'aab86460-0018-44c2-9f52-3a326e58f7f7';
-const PAGE_CONTRIBUTION_ID = '354743b2-f89a-482b-b447-2b5a2367c8bd';
-const STYLE_CONTRIBUTION_ID = '3a3726b8-e0b2-4f10-98ed-b1c82e851b99';
-const SCRIPT_CONTRIBUTION_ID = '5c473463-db07-4132-b8a1-bd40cbdf4b18';
-
-function createName(aLink: string, aBaseName: string): string {
-  const baseName = createBaseName(aLink);
-  return `${aBaseName} - ${baseName}`;
-}
-
-/**
- * Construct a new content item based on the style link
- *
- */
-function createStyleItem(
-  aStyle: HTMLLinkElement,
-  aBaseName: string,
-  aBasePath: string,
-  tags: string[]
-): AuthoringContentItem {
-  // url
-  const href = aStyle.href;
-  const name = createName(href, aBaseName);
-  const id = createGuid(name);
-  const path = `${aBasePath}/${href}`;
-  const key = createGuid(path);
-  // construct the item
-  const item = {
-    id,
-    name,
-    tags,
-    classification: CLASSIFICATION_CONTENT,
-    typeId: STYLE_CONTRIBUTION_ID,
-    elements: {
-      href: {
-        elementType: ELEMENT_TYPE_TEXT,
-        value: path
-      },
-      key: {
-        elementType: ELEMENT_TYPE_TEXT,
-        value: key
-      }
-    },
-    status: Status.READY
-  };
-  // ok
-  return item as any;
-}
-
-/**
- * Construct a new content item based on the style link
- *
- */
-function createScriptItem(
-  aScript: HTMLScriptElement,
-  aBaseName: string,
-  aBasePath: string,
-  tags: string[]
-): AuthoringContentItem {
-  // url
-  const src = aScript.src;
-  const name = createName(src, aBaseName);
-  const id = createGuid(name);
-  const path = `${aBasePath}/${src}`;
-  const key = createGuid(path);
-  // check for the type
-  const selection = aScript.hasAttribute('nomodule')
-    ? 'nomodule'
-    : 'application/javascript';
-  // construct the item
-  const item = {
-    id,
-    name,
-    tags,
-    classification: CLASSIFICATION_CONTENT,
-    typeId: SCRIPT_CONTRIBUTION_ID,
-    elements: {
-      src: {
-        elementType: ELEMENT_TYPE_TEXT,
-        value: path
-      },
-      type: {
-        elementType: ELEMENT_TYPE_OPTION_SELECTION,
-        value: {
-          selection
-        }
-      },
-      key: {
-        elementType: ELEMENT_TYPE_TEXT,
-        value: key
-      }
-    },
-    status: Status.READY
-  };
-  // ok
-  return item as any;
-}
-
-function createHeadArtifacts(
-  aHead: HTMLElement,
-  aBaseName: string,
-  aBasePath: string,
-  tags: string[]
-): Observable<AuthoringContentItem> {
-  // head links
-  const style$ = rxPipe(
-    selectStyleLinks(aHead),
-    map((link) => createStyleItem(link, aBaseName, aBasePath, tags))
-  );
-  // head scripts
-  const script$ = rxPipe(
-    selectScripts(aHead),
-    map((script) => createScriptItem(script, aBaseName, aBasePath, tags))
-  );
-  // merge
-  return merge(style$, script$);
-}
-
-function createBodyArtifacts(
-  aBody: HTMLElement,
-  aBaseName: string,
-  aBasePath: string,
-  tags: string[]
-): Observable<AuthoringContentItem> {
-  // body scripts
-  return rxPipe(
-    selectScripts(aBody),
-    map((script) => createScriptItem(script, aBaseName, aBasePath, tags))
-  );
-}
-
-/**
- * Adds the contribution for the app root element
- *
- * @param selection - the selection mode
- * @returns the element
- */
-function appRootContribution(selection: string) {
-  return {
-    embed: {
-      elementType: ELEMENT_TYPE_TEXT,
-      values: ['<app-root></app-root>']
-    },
-    mode: {
-      elementType: ELEMENT_TYPE_OPTION_SELECTION,
-      value: {
-        selection
-      }
-    }
-  };
-}
-
-interface AuthoringIdRef {
-  id: string;
-  mode: ArtifactMode;
-}
-
-function reference(aItem: ItemWithMode): AuthoringIdRef {
-  return {
-    id: aItem.item.id,
-    mode: aItem.mode
-  };
-}
-
-/**
- * Organize the IDs by mode
- *
- * @param aValues - the authoring IDs
- * @returns the values organized by mode
- */
-function byMode(aValues: AuthoringIdRef[]): Record<string, string[]> {
-  // reduce
-  return reduceArray(
-    aValues,
-    (aDst: Record<string, string[]>, aRef: AuthoringIdRef) => {
-      // insert
-      arrayPush(aRef.id, assertArray(aRef.mode, aDst));
-      return aDst;
-    },
-    {}
-  );
-}
-
-/**
- * Generates a reproducible guid for the configuration
- *
- * @param selection  - selection
- * @param values - reference values
- *
- * @returns the guid
- */
-const createReferenceContributionsGuid = (
-  aSelection: string,
-  aValues: string[]
-) => createGuid([aSelection, ...aValues].join('/'));
-
-function referenceContributions(selection: string, values: string[]) {
-  // generate a key
-  const key = createReferenceContributionsGuid(selection, values);
-  // the reference
-  return {
-    reference: {
-      values: mapArray(values, (id) => ({ id })),
-      elementType: ELEMENT_TYPE_REFERENCE
-    },
-    mode: {
-      elementType: ELEMENT_TYPE_OPTION_SELECTION,
-      value: {
-        selection
-      }
-    },
-    key: {
-      elementType: ELEMENT_TYPE_TEXT,
-      value: key
-    }
-  };
-}
-
-/**
- * Generates a reference contribution from all items on the stream
- *
- * @param aMode - the mode
- * @returns the operation that creates the contribution
- */
-function referenceContribution(): OperatorFunction<ItemWithMode, any> {
-  return (item$) =>
-    rxPipe(
-      item$,
-      map(reference),
-      rxToArray(),
-      map(byMode),
-      map((values) =>
-        reduceForIn(
-          values,
-          (dst: any[], val: string[], mode: string) =>
-            arrayPush(referenceContributions(mode, val), dst),
-          []
-        )
-      ),
-      mergeMap((all) => from(all))
-    );
-}
-
-function createPageContribution(values: any[]) {
-  return {
-    values,
-    elementType: ELEMENT_TYPE_GROUP,
-    typeRef: {
-      id: PAGE_CONTRIBUTION_ID
-    }
-  };
-}
-
-function headContribution(): OperatorFunction<ItemWithMode, any> {
-  return (item$) =>
-    rxPipe(
-      item$,
-      referenceContribution(),
-      rxToArray(),
-      map(createPageContribution)
-    );
-}
-
-function bodyContribution(): OperatorFunction<ItemWithMode, any> {
-  return (item$) =>
-    rxPipe(
-      item$,
-      referenceContribution(),
-      //      startWith(appRootContribution(aMode)),
-      rxToArray(),
-      map(createPageContribution)
-    );
-}
-
-function createPageContributions(
-  aProjectName: string,
-  head: any,
-  body: any,
-  tags: string[]
-): AuthoringContentItem {
-  // generate some ids
-  const name = `${aProjectName} - Profile`;
-  const id = createGuid(name);
-  const key = id;
-
-  const item = {
-    tags,
-    classification: CLASSIFICATION_CONTENT,
-    elements: {
-      head,
-      body,
-      key: {
-        elementType: ELEMENT_TYPE_TEXT,
-        value: key
-      }
-    },
-    name,
-    typeId: PAGE_CONTRIBUTIONS_ID,
-    id,
-    status: Status.READY
-  };
-  // some magic casting
-  return item as any;
-}
-
-function addRevision<T>(aValue: T): T {
-  // patch
-  const value = aValue as any;
-  value.rev = createRevision(aValue);
-  // ok
-  return value;
-}
-
-const DEFAULT_MODE = `${ArtifactMode.LIVE},${ArtifactMode.PREVIEW}`;
-
-const DEFAULT_CONFIGURATION = 'production';
-
-const DEFAULT_TARGET = 'build';
-
-interface ModeConfig {
-  /**
-   * The mode
-   */
-  mode: ArtifactMode;
-  /**
-   * The matching output path
-   */
-  outputPath: string;
-}
+const selectOutputPath = pluckProperty<JSONObject, 'outputPath'>('outputPath');
 
 /**
  * Constructs a config object
@@ -436,6 +62,15 @@ const createModeConfig = (aMode: string, aOptions: JSONObject): ModeConfig => ({
   mode: aMode as ArtifactMode,
   outputPath: ensureDirPath(anyToString(selectOutputPath(aOptions)))
 });
+
+/**
+ * Decodes the configuration from the schema
+ *
+ * @param aSchema - the schema
+ * @returns the configuration
+ */
+const getConfig = (aSchema: CreateNgDriverArtifactsSchema) =>
+  aSchema.configuration || DEFAULT_CONFIGURATION;
 
 /**
  * Returns the configuration options for the project in question
@@ -487,70 +122,39 @@ function getModeConfigs(
   ];
 }
 
-interface ItemWithMode {
-  mode: ArtifactMode;
-  item: AuthoringContentItem;
-}
-
-const createItemWithMode = (
-  mode: ArtifactMode,
-  item: AuthoringContentItem
-): ItemWithMode => ({ mode, item });
-
-interface Artifacts {
-  head$: Observable<ItemWithMode>;
-  body$: Observable<ItemWithMode>;
-}
+/**
+ * Extracts the project from a workspace
+ *
+ * @param aName - the project name
+ * @param aWorkspace - the workspace schema
+ *
+ * @returns the project
+ */
+const getProject = (aName: string, aWorkspace: WorkspaceSchema) =>
+  getPath<WorkspaceProject<ProjectType.Application>>(aWorkspace, [
+    'projects',
+    aName
+  ]);
 
 /**
- * Constructs the artifacts for a particular root
+ * Decodes the modes from the schema
  *
- * @param aConfig - the config
- * @param aRootDir  - root directory
- * @param aProjectName - name of the project
- * @param aTags - the tags for the artifacts
- * @param aReadTextFile - read callback
- *
- * @returns the contributions
+ * @param aSchema - the schema
+ * @returns the modes
  */
-function createArtifacts(
-  aConfig: ModeConfig,
-  aRootDir: string,
-  aProjectName: string,
-  aTags: string[],
-  aReadTextFile: ReadTextFile
-): Artifacts {
-  // decode the config
-  const { mode, outputPath } = aConfig;
-  // the base name
-  const baseName = `${aProjectName} - ${mode}`;
-  // index
-  const indexPath = `${aRootDir}${outputPath}/index.html`;
-  // parse the index
-  const dom$ = rxPipe(
-    aReadTextFile(indexPath),
-    map((data) => new JSDOM(data)),
-    map((jsdom) => jsdom.window.document),
-    opShareLast
+const getModes = (aSchema: CreateNgDriverArtifactsSchema) =>
+  splitArray(aSchema.mode || DEFAULT_MODE);
+
+/**
+ * Decodes the tags
+ */
+const getTags = (
+  aSchema: CreateNgDriverArtifactsSchema,
+  aProjectName: string
+) =>
+  Array.from(
+    new Set([...splitArray(aSchema.tag || ''), aProjectName, 'sites-next'])
   );
-  // head and body contributions
-  const head$ = rxPipe(
-    dom$,
-    pluck('head'),
-    mergeMap((el) => createHeadArtifacts(el, baseName, outputPath, aTags)),
-    map((item) => createItemWithMode(mode, item)),
-    shareReplay()
-  );
-  const body$ = rxPipe(
-    dom$,
-    pluck('body'),
-    mergeMap((el) => createBodyArtifacts(el, baseName, outputPath, aTags)),
-    map((item) => createItemWithMode(mode, item)),
-    shareReplay()
-  );
-  // returns the artifacts
-  return { head$, body$ };
-}
 
 function createArtifactsForProject(
   aProject: WorkspaceProject<ProjectType.Application>,
@@ -590,75 +194,6 @@ function createArtifactsForProject(
     merge(raw$, pageContribution$),
     map(canonicalizeJson),
     map(addRevision)
-  );
-}
-
-/**
- * Extracts the project from a workspace
- *
- * @param aName - the project name
- * @param aWorkspace - the workspace schema
- *
- * @returns the project
- */
-const getProject = (aName: string, aWorkspace: WorkspaceSchema) =>
-  getPath<WorkspaceProject<ProjectType.Application>>(aWorkspace, [
-    'projects',
-    aName
-  ]);
-
-/**
- * Splits the array into individual values
- *
- * @param aValue - the value, separated by ','
- * @returns the split array
- */
-const splitArray = (aValue: string): string[] =>
-  filterArray(
-    mapArray(aValue.split(','), (m) => m.trim()),
-    isNotEmpty
-  );
-
-/**
- * Decodes the modes from the schema
- *
- * @param aSchema - the schema
- * @returns the modes
- */
-const getModes = (aSchema: CreateNgDriverArtifactsSchema) =>
-  splitArray(aSchema.mode || DEFAULT_MODE);
-
-/**
- * Decodes the tags
- */
-const getTags = (
-  aSchema: CreateNgDriverArtifactsSchema,
-  aProjectName: string
-) =>
-  Array.from(
-    new Set([...splitArray(aSchema.tag || ''), aProjectName, 'sites-next'])
-  );
-
-/**
- * Decodes the configuration from the schema
- *
- * @param aSchema - the schema
- * @returns the configuration
- */
-const getConfig = (aSchema: CreateNgDriverArtifactsSchema) =>
-  aSchema.configuration || DEFAULT_CONFIGURATION;
-
-function readFilesForConfig(
-  aRoot: string,
-  aConfig: ModeConfig,
-  aReadDir: ReadDirectory
-): Observable<FileDescriptor<Buffer>> {
-  // target directory
-  const outDir = `${aRoot}${aConfig.outputPath}`;
-  // scan the files
-  return rxPipe(
-    aReadDir(outDir),
-    map(([name, buffer]) => createFileDescriptor(`${outDir}${name}`, buffer))
   );
 }
 
