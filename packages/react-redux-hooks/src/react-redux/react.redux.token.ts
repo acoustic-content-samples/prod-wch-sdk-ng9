caeed64f776501302b82730ba77dd3169f33dbcb
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-01-31 12:39:12 +0100

    fix typings

--- packages/react-redux-hooks/src/react-redux/react.redux.token.ts
+++ packages/react-redux-hooks/src/react-redux/react.redux.token.ts
@@ -1,22 +1,6 @@
-import { Context } from 'react';
 import { ReactReduxContext } from 'react-redux';
-import { Store } from 'redux';
-
-/**
- * Reverse engineering of the correct type of the `ReactReduxContext`.
- */
-export interface ReactReduxContextType {
-  /**
-   * The redux store
-   */
-  store: Store;
-  /**
-   * some magic subscription, I could not find any doc
-   */
-  subscription: any;
-}
 
 /**
  * Injection token for the {@link https://react-redux.js.org/|react redux} store
  */
-export const WCH_CONTEXT_REACT_REDUX: Context<ReactReduxContextType> = ReactReduxContext;
+export const WCH_CONTEXT_REACT_REDUX = ReactReduxContext;
