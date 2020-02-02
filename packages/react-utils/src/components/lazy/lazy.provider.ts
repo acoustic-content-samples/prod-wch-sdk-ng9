38441d4f09805f92d8ea9c5f8e7a765dd617b0a5
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-02-02 21:26:43 +0100

    remove unneccessary dependency

--- packages/react-utils/src/components/lazy/lazy.provider.ts
+++ packages/react-utils/src/components/lazy/lazy.provider.ts
@@ -1,10 +1,11 @@
-import { ReactComponent } from '@acoustic-content-sdk/react-api';
 import { rxPipe } from '@acoustic-content-sdk/utils';
-import { createElement } from 'react';
+import { ComponentClass, createElement, FunctionComponent } from 'react';
 import { combineLatest, from, ObservableInput } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { rxComponent } from './../reactive/rx.hoc';
+
+declare type CmpInput<T> = FunctionComponent<T> | ComponentClass<T>;
 
 /**
  * Function that can be reused independent of the outer closure
@@ -18,8 +19,8 @@ import { rxComponent } from './../reactive/rx.hoc';
 const internalCreateLazyComponent = (
   req: any[],
   opt: any[],
-  aCreator: ObservableInput<(req: any[], opt: any[]) => ReactComponent<any>>
-): ReactComponent<any> => {
+  aCreator: ObservableInput<(req: any[], opt: any[]) => CmpInput<any>>
+): CmpInput<any> => {
   // lazy load the component
   const Delegate$ = rxPipe(
     from(aCreator),
@@ -45,43 +46,43 @@ const internalCreateLazyComponent = (
  * @returns the sync component creator
  */
 const internalLazyComponent = (
-  aCreator: ObservableInput<(req: any[], opt: any[]) => ReactComponent<any>>
-): ((req: any[], opt: any[]) => ReactComponent<any>) => (
+  aCreator: ObservableInput<(req: any[], opt: any[]) => CmpInput<any>>
+): ((req: any[], opt: any[]) => CmpInput<any>) => (
   req: any[],
   opt: any[]
-): ReactComponent<any> => internalCreateLazyComponent(req, opt, aCreator);
+): CmpInput<any> => internalCreateLazyComponent(req, opt, aCreator);
 
 // prettier-ignore
 export interface LazyComponentCreator {
   // no dependencies
-  <T>(fct: ObservableInput<(req?: never, opt?: never) => ReactComponent<T>>): (req?: never, opt?: never) => ReactComponent<T>;
+  <T>(fct: ObservableInput<(req?: never, opt?: never) => CmpInput<T>>): (req?: never, opt?: never) => CmpInput<T>;
   // only required dependencies
-  <R1, T>(fct: ObservableInput<(req: [R1], opt?: never) => ReactComponent<T>>): (req: [R1], opt?: never) => ReactComponent<T>;
-  <R1, R2, T>(fct: ObservableInput<(req: [R1, R2], opt?: never) => ReactComponent<T>>):  (req: [R1, R2], opt?: never) => ReactComponent<T>;
-  <R1, R2, R3, T>(fct: ObservableInput<(req: [R1, R2, R3], opt?: never) => ReactComponent<T>>): (req: [R1, R2, R3], opt?: never) => ReactComponent<T>;
-  <R1, R2, R3, R4, T>(fct: ObservableInput<(req: [R1, R2, R3, R4], opt?: never) => ReactComponent<T>>): (req: [R1, R2, R3, R4], opt?: never) => ReactComponent<T>;
+  <R1, T>(fct: ObservableInput<(req: [R1], opt?: never) => CmpInput<T>>): (req: [R1], opt?: never) => CmpInput<T>;
+  <R1, R2, T>(fct: ObservableInput<(req: [R1, R2], opt?: never) => CmpInput<T>>):  (req: [R1, R2], opt?: never) => CmpInput<T>;
+  <R1, R2, R3, T>(fct: ObservableInput<(req: [R1, R2, R3], opt?: never) => CmpInput<T>>): (req: [R1, R2, R3], opt?: never) => CmpInput<T>;
+  <R1, R2, R3, R4, T>(fct: ObservableInput<(req: [R1, R2, R3, R4], opt?: never) => CmpInput<T>>): (req: [R1, R2, R3, R4], opt?: never) => CmpInput<T>;
   // only optional dependencies
-  <O1, T>(fct: ObservableInput<(req: never, opt:[O1?]) => ReactComponent<T>>): (req: never, opt:[O1?]) => ReactComponent<T>;
-  <O1, O2, T>(fct: ObservableInput<(req: never, opt:[O1?, O2?]) => ReactComponent<T>>): (req: never, opt:[O1?, O2?]) => ReactComponent<T>;
-  <O1, O2, O3, T>(fct: ObservableInput<(req: never, opt:[O1?, O2?, O3?]) => ReactComponent<T>>): (req: never, opt:[O1?, O2?, O3?]) => ReactComponent<T>;
-  <O1, T>(fct: ObservableInput<(req: [], opt:[O1?]) => ReactComponent<T>>): (req: [], opt:[O1?]) => ReactComponent<T>;
-  <O1, O2, T>(fct: ObservableInput<(req: [], opt:[O1?, O2?]) => ReactComponent<T>>): (req: [], opt:[O1?, O2?]) => ReactComponent<T>;
-  <O1, O2, O3, T>(fct: ObservableInput<(req: [], opt:[O1?, O2?, O3?]) => ReactComponent<T>>): (req: [], opt:[O1?, O2?, O3?]) => ReactComponent<T>;
+  <O1, T>(fct: ObservableInput<(req: never, opt:[O1?]) => CmpInput<T>>): (req: never, opt:[O1?]) => CmpInput<T>;
+  <O1, O2, T>(fct: ObservableInput<(req: never, opt:[O1?, O2?]) => CmpInput<T>>): (req: never, opt:[O1?, O2?]) => CmpInput<T>;
+  <O1, O2, O3, T>(fct: ObservableInput<(req: never, opt:[O1?, O2?, O3?]) => CmpInput<T>>): (req: never, opt:[O1?, O2?, O3?]) => CmpInput<T>;
+  <O1, T>(fct: ObservableInput<(req: [], opt:[O1?]) => CmpInput<T>>): (req: [], opt:[O1?]) => CmpInput<T>;
+  <O1, O2, T>(fct: ObservableInput<(req: [], opt:[O1?, O2?]) => CmpInput<T>>): (req: [], opt:[O1?, O2?]) => CmpInput<T>;
+  <O1, O2, O3, T>(fct: ObservableInput<(req: [], opt:[O1?, O2?, O3?]) => CmpInput<T>>): (req: [], opt:[O1?, O2?, O3?]) => CmpInput<T>;
   // one required and optional dependencies
-  <R1, O1, T>(fct: ObservableInput<(req: [R1], opt:[O1?]) => ReactComponent<T>>): (req: [R1], opt:[O1?]) => ReactComponent<T>;
-  <R1, O1, O2, T>(fct: ObservableInput<(req: [R1], opt:[O1?, O2?]) => ReactComponent<T>>): (req: [R1], opt:[O1?, O2?]) => ReactComponent<T>;
+  <R1, O1, T>(fct: ObservableInput<(req: [R1], opt:[O1?]) => CmpInput<T>>): (req: [R1], opt:[O1?]) => CmpInput<T>;
+  <R1, O1, O2, T>(fct: ObservableInput<(req: [R1], opt:[O1?, O2?]) => CmpInput<T>>): (req: [R1], opt:[O1?, O2?]) => CmpInput<T>;
   // two required and optional dependencies
-  <R1, R2, O1, T>(fct: ObservableInput<(req: [R1, R2], opt:[O1?]) => ReactComponent<T>>): (req: [R1, R2], opt:[O1?]) => ReactComponent<T>;
-  <R1, R2, O1, O2, T>(fct: ObservableInput<(req: [R1, R2], opt:[O1?, O2?]) => ReactComponent<T>>): (req: [R1, R2], opt:[O1?, O2?]) => ReactComponent<T>;
+  <R1, R2, O1, T>(fct: ObservableInput<(req: [R1, R2], opt:[O1?]) => CmpInput<T>>): (req: [R1, R2], opt:[O1?]) => CmpInput<T>;
+  <R1, R2, O1, O2, T>(fct: ObservableInput<(req: [R1, R2], opt:[O1?, O2?]) => CmpInput<T>>): (req: [R1, R2], opt:[O1?, O2?]) => CmpInput<T>;
   // three required and optional dependencies
-  <R1, R2, R3, O1, T>(fct: ObservableInput<(req: [R1, R2, R3], opt:[O1?]) => ReactComponent<T>>): (req: [R1, R2, R3], opt:[O1?]) => ReactComponent<T>;
-  <R1, R2, R3, O1, O2, T>(fct: ObservableInput<(req: [R1, R2, R3], opt:[O1?, O2?]) => ReactComponent<T>>): (req: [R1, R2, R3], opt:[O1?, O2?]) => ReactComponent<T>;
+  <R1, R2, R3, O1, T>(fct: ObservableInput<(req: [R1, R2, R3], opt:[O1?]) => CmpInput<T>>): (req: [R1, R2, R3], opt:[O1?]) => CmpInput<T>;
+  <R1, R2, R3, O1, O2, T>(fct: ObservableInput<(req: [R1, R2, R3], opt:[O1?, O2?]) => CmpInput<T>>): (req: [R1, R2, R3], opt:[O1?, O2?]) => CmpInput<T>;
   // four required and optional dependencies
-  <R1, R2, R3, R4, O1, T>(fct: ObservableInput<(req: [R1, R2, R3, R4], opt:[O1?]) => ReactComponent<T>>): (req: [R1, R2, R3, R4], opt:[O1?]) => ReactComponent<T>;
+  <R1, R2, R3, R4, O1, T>(fct: ObservableInput<(req: [R1, R2, R3, R4], opt:[O1?]) => CmpInput<T>>): (req: [R1, R2, R3, R4], opt:[O1?]) => CmpInput<T>;
   // five required and optional dependencies
-  <R1, R2, R3, R4, R5, O1, T>(fct: ObservableInput<(req: [R1, R2, R3, R4, R5], opt:[O1?]) => ReactComponent<T>>): (req: [R1, R2, R3, R4, R5], opt:[O1?]) => ReactComponent<T>;
+  <R1, R2, R3, R4, R5, O1, T>(fct: ObservableInput<(req: [R1, R2, R3, R4, R5], opt:[O1?]) => CmpInput<T>>): (req: [R1, R2, R3, R4, R5], opt:[O1?]) => CmpInput<T>;
   // six required and optional dependencies
-  <R1, R2, R3, R4, R5, R6, O1, T>(fct: ObservableInput<(req: [R1, R2, R3, R4, R5, R6], opt:[O1?]) => ReactComponent<T>>): (req: [R1, R2, R3, R4, R5, R6], opt:[O1?]) => ReactComponent<T>;
+  <R1, R2, R3, R4, R5, R6, O1, T>(fct: ObservableInput<(req: [R1, R2, R3, R4, R5, R6], opt:[O1?]) => CmpInput<T>>): (req: [R1, R2, R3, R4, R5, R6], opt:[O1?]) => CmpInput<T>;
 }
 
 /**
