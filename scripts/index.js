cba9ad58b952bac346c89ba8af3af113fb166151
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-02-03 16:40:44 +0100

    fix workdir

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,25 @@
 
 const { readJson, writeJson } = require('fs-extra');
 const { join } = require('path');
+const { chdir, cwd } = require('process');
 const spawn = require('cross-spawn');
+
+const currentDir = cwd();
+const workDir = __dirname;
+
+const toWorkDir = () => chdir(workDir);
+const toCurrentDir = () => chdir(currentDir);
 
 console.log('Carsten', __dirname);
 
-const pkg = join(__dirname, 'package.json');
+const pkg = join(workDir, 'package.json');
 const pkg$ = readJson(pkg)
   .then((data) => ({ ...data, private: true }))
   .then((data) => writeJson(pkg, data));
 
-const pub$ = pkg$.then(() =>
-  spawn('yarn', ['run', 'publish'], { stdio: 'inherit', cwd: __dirname })
-);
+const pub$ = pkg$
+  .then(toWorkDir)
+  .then(() =>
+    spawn('yarn', ['run', 'publish'], { stdio: 'inherit', cwd: workDir })
+  )
+  .then(toCurrentDir, toCurrentDir);
