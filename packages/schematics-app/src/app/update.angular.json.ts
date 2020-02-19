4e28bc28003a7d558ffd83399d6d4ccbbb140e71
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-02-19 10:33:37 +0100

    add modes to serve config

--- packages/schematics-app/src/app/update.angular.json.ts
+++ packages/schematics-app/src/app/update.angular.json.ts
@@ -1,4 +1,3 @@
-import { parse } from 'path';
 import {
   findProjectName,
   getWorkspacePath,
@@ -13,15 +12,18 @@ import {
   rxPipe
 } from '@acoustic-content-sdk/utils';
 import { Rule, Tree } from '@angular-devkit/schematics';
+import { parse } from 'path';
 import { Observable, of } from 'rxjs';
 import { endWith, ignoreElements } from 'rxjs/operators';
 
 import { addModeToName, addModeToPath } from '../utilities/names';
 import { Schema } from './schema';
-import { ensureDirPath } from '@acoustic-content-sdk/tooling';
 
 const selectBuild = (aName: string) =>
   pluckPath<string>(['projects', aName, 'architect', 'build']);
+
+const selectServe = (aName: string) =>
+  pluckPath<string>(['projects', aName, 'architect', 'serve']);
 
 const KEY_SOURCE_MAP = 'sourceMap';
 export const CONFIG_SOURCE_MAP = KEY_SOURCE_MAP;
@@ -30,13 +32,21 @@ export const KEY_OUTPUT_PATH = 'outputPath';
 export const KEY_MAIN = 'main';
 export const KEY_POLYFILLS = 'polyfills';
 export const KEY_TS_CONFIG = 'tsConfig';
+const KEY_BROWSER_TARGET = 'browserTarget';
 
 const NAME_TSCONFIG = 'tsconfig.json';
 
 const selectConfigurations = pluckPath<Record<string, any>>(['configurations']);
 const selectOptions = pluckPath<Record<string, any>>(['options']);
 
-function createModeConfig(aMode: ArtifactMode, aOptions: any): any {
+function createModeConfigServe(aMode: ArtifactMode, aProjectName: string): any {
+  // transform
+  return {
+    [KEY_BROWSER_TARGET]: `${aProjectName}:build:${aMode}`
+  };
+}
+
+function createModeConfigBuild(aMode: ArtifactMode, aOptions: any): any {
   // names
   const outputPath = addModeToPath(aOptions[KEY_OUTPUT_PATH], aMode);
   const main = addModeToName(aOptions[KEY_MAIN], aMode);
@@ -51,21 +61,35 @@ function createModeConfig(aMode: ArtifactMode, aOptions: any): any {
   };
 }
 
-function updateModeConfig(
+function updateModeConfigBuild(
   aMode: ArtifactMode,
   aOptions: any,
-  aConfigurations: any
+  aConfigurations: Record<string, any>
 ): any {
   // check if we already have a config
   const existingConfig = aConfigurations[aMode];
   if (isNil(existingConfig)) {
     // update
-    aConfigurations[aMode] = createModeConfig(aMode, aOptions);
+    aConfigurations[aMode] = createModeConfigBuild(aMode, aOptions);
   }
   return aConfigurations;
 }
 
-function updateSourceMap(aConfigurations: any): any {
+function updateModeConfigServe(
+  aMode: ArtifactMode,
+  aProjectName: string,
+  aConfigurations: Record<string, any>
+): any {
+  // check if we already have a config
+  const existingConfig = aConfigurations[aMode];
+  if (isNil(existingConfig)) {
+    // update
+    aConfigurations[aMode] = createModeConfigServe(aMode, aProjectName);
+  }
+  return aConfigurations;
+}
+
+function updateSourceMap(aConfigurations: Record<string, any>): any {
   // check if we already have a config
   const existingConfig = aConfigurations[CONFIG_SOURCE_MAP];
   if (isNil(existingConfig)) {
@@ -84,13 +108,18 @@ function transformAngularJson(
   const ws: WorkspaceSchema = cloneDeep(aWorkspace);
   // build root
   const build = selectBuild(aProjectName)(ws);
+  const serve = selectServe(aProjectName)(ws);
   // the configs
   const options = selectOptions(build);
-  const config = selectConfigurations(build);
-  // add for modes
-  updateModeConfig(ArtifactMode.LIVE, options, config);
-  updateModeConfig(ArtifactMode.PREVIEW, options, config);
-  updateSourceMap(config);
+  const buildConfig = selectConfigurations(build);
+  const serveConfig = selectConfigurations(serve);
+  // add build config for modes
+  updateModeConfigBuild(ArtifactMode.LIVE, options, buildConfig);
+  updateModeConfigBuild(ArtifactMode.PREVIEW, options, buildConfig);
+  updateSourceMap(buildConfig);
+  // add serve config for modes
+  updateModeConfigServe(ArtifactMode.LIVE, aProjectName, serveConfig);
+  updateModeConfigServe(ArtifactMode.PREVIEW, aProjectName, serveConfig);
   // returns the update
   return of(ws);
 }
