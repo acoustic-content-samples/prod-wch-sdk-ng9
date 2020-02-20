e110c090f6455c99814432da2eedb5bb0b535a2d
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-02-20 13:50:18 +0100

    fix api build

--- packages/schematics-bootstrap/src/bootstrap/bootstrap.ts
+++ packages/schematics-bootstrap/src/bootstrap/bootstrap.ts
@@ -20,6 +20,9 @@ const selectIndexFile = (aName: string) =>
     'index'
   ]);
 
+/**
+ * See {@link https://getbootstrap.com/docs/4.4/getting-started/introduction/|Bootstrap doc}
+ */
 const BOOSTRAP_CSS = `<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">`;
 const BOOTSTRAP_SCRIPT = `
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
@@ -27,6 +30,12 @@ const BOOTSTRAP_SCRIPT = `
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 `;
 
+/**
+ * Transforms the document by adding bootstrap
+ *
+ * @param aFragment - the fragment to modify
+ * @returns the modified document
+ */
 function transformIndex(
   aFragment: DocumentFragment
 ): Observable<DocumentFragment> {
@@ -34,11 +43,11 @@ function transformIndex(
   if (isNil(aFragment)) {
     return of(aFragment);
   }
-  // some helper magic
-  const tmp = aFragment.ownerDocument.createElement('div');
   // find the head and body sections
   const head = aFragment.querySelector('head');
   const body = aFragment.querySelector('body');
+  // some helper magic
+  const tmp = aFragment.ownerDocument.createElement('div');
   // add link
   tmp.innerHTML = BOOSTRAP_CSS;
   const links = tmp.querySelectorAll('link');
