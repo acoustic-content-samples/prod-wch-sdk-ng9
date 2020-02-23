9b4925652d5f285ffde011e3b6ff13c95b160181
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-02-23 17:35:37 +0100

    prepare ng-hbs-view

--- packages/ng-app-live/src/modules/live.module.ts
+++ packages/ng-app-live/src/modules/live.module.ts
@@ -2,6 +2,7 @@ import { LoggerService, WchSdkVersion } from '@acoustic-content-sdk/api';
 import { ACOUSTIC_TOKEN_LOGGER_SERVICE } from '@acoustic-content-sdk/ng-api';
 import { WchNgRestModule } from '@acoustic-content-sdk/ng-rest';
 import { WchNgEditDirectivesModule } from '@acoustic-content-sdk/ng-view';
+import { WchNgHbsViewModule } from '@acoustic-content-sdk/ng-hbs-view';
 import { logModule } from '@acoustic-content-sdk/utils';
 import { Inject, NgModule, Optional } from '@angular/core';
 
@@ -11,7 +12,7 @@ import { MODULE, VERSION } from './../version';
  * Imports the modules required for live functionality
  */
 @NgModule({
-  imports: [WchNgRestModule, WchNgEditDirectivesModule]
+  imports: [WchNgRestModule, WchNgEditDirectivesModule, WchNgHbsViewModule]
 })
 export class WchNgAppLiveModule {
   /**
