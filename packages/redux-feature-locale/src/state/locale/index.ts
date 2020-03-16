77709575f194f43adf8e38f6998d390aaefa1315
Taras Turchyn <taras.turchyn@acoustic.co>
2020-03-16 20:32:41 +0100

    redux-feature-locale - add setLocaleAction to export

--- packages/redux-feature-locale/src/state/locale/index.ts
+++ packages/redux-feature-locale/src/state/locale/index.ts
@@ -1,4 +1,5 @@
 export * from './locale.feature';
 export { LOCALE_FEATURE } from './locale.id';
 export * from './locale.module';
+export { setLocaleAction } from './locale.actions';
 export { localeReducer, LocaleState } from './locale.state';
