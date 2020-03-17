b17437443c78c9e2360254154b863de4c50c42f0
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-03-17 21:52:52 +0100

    organize imports

--- packages/redux-feature-locale/src/state/locale/index.ts
+++ packages/redux-feature-locale/src/state/locale/index.ts
@@ -1,5 +1,5 @@
+export { setLocaleAction } from './locale.actions';
 export * from './locale.feature';
 export { LOCALE_FEATURE } from './locale.id';
 export * from './locale.module';
-export { setLocaleAction } from './locale.actions';
 export { localeReducer, LocaleState } from './locale.state';
