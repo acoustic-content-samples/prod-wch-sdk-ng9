fcf4f357a8b32d8883851ef32855be2ea0f78974
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-03-26 21:37:14 +0100

    add sample fonts

--- packages/tooling-fonts/src/utils/font.face.spec.ts
+++ packages/tooling-fonts/src/utils/font.face.spec.ts
@@ -1,10 +1,48 @@
 import { join } from 'path';
 import { ASSET_ROOT } from './../test/assets';
+import {
+  rxReadJsonFile,
+  rxReadBinaryFile,
+  rxDownloadFile
+} from '@acoustic-content-sdk/rx-utils';
+import { kebabCase, rxPipe } from '@acoustic-content-sdk/utils';
+import { createLoader } from './loader';
+import { createGuidGenerator } from './name';
+import { Observable } from 'rxjs';
+import { FontDetails } from './webfont.helper';
+import { mergeMap, map, mergeMapTo, catchError } from 'rxjs/operators';
+import { createFontFaces } from './font.face';
 
 describe('fontface', () => {
   const ROOT = join(ASSET_ROOT, 'fonts');
 
+  // the load callback
+  const loadBinary = (aUrl: string) => {
+    // local path
+    const path = join(ROOT, 'roboto', kebabCase(aUrl));
+    // try to read locally
+    const local$ = rxReadBinaryFile(path);
+    // fallback
+    const fallback = () =>
+      rxPipe(rxDownloadFile(aUrl, path), mergeMapTo(local$));
+    // return
+    return rxPipe(local$, catchError(fallback));
+  };
+
   it('should generate a fontface definition', () => {
-    console.log(ROOT);
+    // loader
+    const loader = createLoader(loadBinary);
+    const guids = createGuidGenerator(loader);
+    // read the font details
+    const fontDetails$: Observable<FontDetails> = rxReadJsonFile(
+      join(ROOT, 'roboto.json')
+    );
+    // create the details
+    const fontFace$ = rxPipe(
+      fontDetails$,
+      mergeMap((details) => createFontFaces(details, guids))
+    );
+
+    return rxPipe(fontFace$, map(console.log)).toPromise();
   });
 });
