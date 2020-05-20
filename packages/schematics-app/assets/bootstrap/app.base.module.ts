d5427841520fe6f5f158f4421d59d061a03632c1
David Herbst <daherbst@us.ibm.com>
2020-05-20 09:45:07 -0600

    dynamically set the base URL

--- packages/schematics-app/assets/bootstrap/app.base.module.ts
+++ packages/schematics-app/assets/bootstrap/app.base.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
 
-import { {{{ACOUSTIC_APP_MODULE}}} as WchAppModule } from '{{{ACOUSTIC_APP_MODULE_PATH}}}';
-import { {{{ORIGINAL_APP_MODULE}}} as OriginalAppModule } from '{{{ORIGINAL_APP_MODULE_PATH}}}';
+import { {{ { ACOUSTIC_APP_MODULE } }} as WchAppModule } from '{{{ACOUSTIC_APP_MODULE_PATH}}}';
+import { {{ { ORIGINAL_APP_MODULE } }} as OriginalAppModule } from '{{{ORIGINAL_APP_MODULE_PATH}}}';
+
+/**
+ * dynamically generates the base URL
+ *
+ * @returns the base URL
+ */
+function getAppBase() {
+  const dxSites = 'dxsites';
+  const siteIdRegexStr = '[\\w\\d_\\-%]';
+  const tenantIdRegexStr = '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}';
+  const baseUrlRegex = new RegExp(`^(?:\\/api)?(?:\\/(${tenantIdRegexStr}))?(?:(?:\\/${dxSites}\\/)(${siteIdRegexStr}+))?(?:\\/)?(?:.*)$`);
+  const [total, tenantId, siteId] = baseUrlRegex.exec(document.location.pathname);
+  const resourceUrl = tenantId ? `/${tenantId}/` : '/';
+
+  const baseUrl = siteId ? resourceUrl + 'dxsites/' + siteId + '/' : resourceUrl;
+
+  return baseUrl;
+}
 
 /**
  * This {@link https://angular.io/guide/architecture-modules|ngModule} imports the SDK features
@@ -12,6 +31,9 @@ import { {{{ORIGINAL_APP_MODULE}}} as OriginalAppModule } from '{{{ORIGINAL_APP_
   imports: [
     WchAppModule,
     OriginalAppModule
+  ],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: getAppBase() }
   ]
 })
-export class AppModule {}
+export class AppModule { }
