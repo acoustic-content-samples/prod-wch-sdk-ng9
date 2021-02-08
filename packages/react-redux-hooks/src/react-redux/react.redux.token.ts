11da463bbf24306c11acb666f3980ecc1e67f70f
lukasz-reszka <66356873+lukasz-reszka@users.noreply.github.com>
2021-02-08 10:05:13 +0100

    CONT-3878 explicit type type

--- packages/react-redux-hooks/src/react-redux/react.redux.token.ts
+++ packages/react-redux-hooks/src/react-redux/react.redux.token.ts
@@ -1,6 +1,7 @@
-import { ReactReduxContext } from 'react-redux';
+import {ReactReduxContext, ReactReduxContextValue} from 'react-redux';
+import {Context} from "react";
 
 /**
  * Injection token for the {@link https://react-redux.js.org/|react redux} store
  */
-export const ACOUSTIC_CONTEXT_REACT_REDUX = ReactReduxContext;
+export const ACOUSTIC_CONTEXT_REACT_REDUX: Context<ReactReduxContextValue> = ReactReduxContext;
